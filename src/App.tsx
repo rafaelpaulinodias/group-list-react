22f6dd30a56748a5983b5ad41bf13599a26299a5
Rafael Dias <rafael.paulinodias@gmail.com>
2021-04-13 17:13:38 -0300

    add showlist for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import ShowList from './pages/show-list';
+
 function App() {
   return (
     <div className="App">
-      
+      <ShowList />
     </div>
   );
 }
