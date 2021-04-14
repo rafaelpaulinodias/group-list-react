a19d3bed50815879503d2d7d1040d6d10617746d
Rafael Dias <rafael.paulinodias@gmail.com>
2021-04-13 21:14:55 -0300

    Revert "add showlist for test"
    This reverts commit 22f6dd30a56748a5983b5ad41bf13599a26299a5.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import ShowList from './pages/show-list';
-
 function App() {
   return (
     <div className="App">
-      <ShowList />
+      
     </div>
   );
 }
